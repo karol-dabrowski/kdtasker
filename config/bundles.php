985057d6acdc819fd3c8099142f50a1ce912bbe9
Karol <dabrowski.karad@gmail.com>
2019-03-12 17:31:04 +0100

    Add service bus bundle and configure command bus

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Prooph\Bundle\ServiceBus\ProophServiceBusBundle::class => ['all' => true],
 ];

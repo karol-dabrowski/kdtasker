049f17d3fa8268e2ac13da509e70cebc91f92fa0
Karol <dabrowski.karad@gmail.com>
2019-05-16 15:34:58 +0200

    Add requester user id to all commands in ApiController

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,10 +4,12 @@ declare(strict_types = 1);
 namespace App\Controller;
 
 use App\Util\Response\ErrorResponseFactory;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Prooph\Common\Messaging\MessageFactory;
 use Prooph\ServiceBus\CommandBus;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 /**
  * Class ApiController
@@ -25,17 +27,35 @@ final class ApiController
 	 */
 	private $messageFactory;
 
+	/**
+	 * @var TokenStorageInterface
+	 */
+	private $tokenStorageInterface;
+
+	/**
+	 * @var JWTTokenManagerInterface
+	 */
+	private $jwtManager;
+
 	private const NAME_ATTRIBUTE = 'command';
 
 	/**
 	 * ApiController constructor.
 	 * @param CommandBus $commandBus
 	 * @param MessageFactory $messageFactory
+	 * @param TokenStorageInterface $tokenStorageInterface
+	 * @param JWTTokenManagerInterface $jwtManager
 	 */
-	public function __construct(CommandBus $commandBus, MessageFactory $messageFactory)
-	{
+	public function __construct(
+		CommandBus $commandBus,
+		MessageFactory $messageFactory,
+		TokenStorageInterface $tokenStorageInterface,
+		JWTTokenManagerInterface $jwtManager
+	) {
 		$this->commandBus = $commandBus;
 		$this->messageFactory = $messageFactory;
+		$this->tokenStorageInterface = $tokenStorageInterface;
+		$this->jwtManager = $jwtManager;
 	}
 
 	/**
@@ -44,9 +64,13 @@ final class ApiController
 	 */
 	public function postAction(Request $request)
 	{
+		$decodedJwtToken = $this->jwtManager->decode($this->tokenStorageInterface->getToken());
+		$userId = $decodedJwtToken['user_id'];
+
 		$data = json_decode($request->getContent(), true);
 		$commandName = $request->attributes->get(self::NAME_ATTRIBUTE);
 		$payload = ['payload' => $data['payload']];
+		$payload['payload']['user_id'] = $userId;
 
 		try {
 			$command = $this->messageFactory->createMessageFromArray($commandName, $payload);

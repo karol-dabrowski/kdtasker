d9ca477f1aed768619d7fc2e3f5e0f9c10f6f3b6
Karol <dabrowski.karad@gmail.com>
2019-05-17 13:39:37 +0200

    Fix task unit test

--- tests/unit/Task/TaskTest.php
+++ tests/unit/Task/TaskTest.php
@@ -6,6 +6,7 @@ namespace App\Tests\Unit\Task;
 use Tasker\Model\Task\Domain\Task;
 use Tasker\Model\Task\Domain\TaskId;
 use Tasker\Model\Task\Event\TaskCreated;
+use Tasker\Model\User\Domain\UserId;
 
 /**
  * Class TaskTest
@@ -29,19 +30,34 @@ class TaskTest extends \Codeception\Test\Unit
     private $taskTitle;
 
 	/**
+	 * @var UserId
+	 */
+	private $creatorId;
+
+	/**
+	 * @var UserId
+	 */
+	private $assigneeId;
+
+	/**
 	 * @throws \Exception
 	 */
 	protected function _before()
 	{
 		$this->taskId = TaskId::generate();
 		$this->taskTitle = "Test title";
+		$this->creatorId = UserId::generate();
+		$this->assigneeId = UserId::generate();
+
 	}
 
 	public function testTaskCanBeCreated()
     {
-		$task = Task::create($this->taskId, $this->taskTitle);
+		$task = Task::create($this->taskId, $this->taskTitle, $this->creatorId, $this->assigneeId);
 	    $this->assertSame($this->taskId, $task->taskId());
 	    $this->assertSame($this->taskTitle, $task->title());
+	    $this->assertSame($this->creatorId, $task->creatorId());
+	    $this->assertSame($this->assigneeId, $task->assigneeId());
 	    $this->assertInstanceOf(Task::class, $task);
 
 		$events = $this->tester->popRecordedEvents($task);
@@ -51,5 +67,7 @@ class TaskTest extends \Codeception\Test\Unit
 		$this->assertInstanceOf(TaskCreated::class, $event);
 	    $this->assertSame($this->taskId, $event->taskId());
 	    $this->assertSame($this->taskTitle, $event->title());
+		$this->assertSame($this->creatorId, $event->creatorId());
+		$this->assertSame($this->assigneeId, $event->assigneeId());
     }
 }

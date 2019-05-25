390cec4c0def9c8ab3d97d71ccf4e697715638c3
Karol <dabrowski.karad@gmail.com>
2019-05-25 19:43:49 +0200

    Create task finder

---
+++ src/Tasker/Infrastructure/Projection/Task/TaskFinder.php
@@ -0,0 +1,76 @@
+<?php
+declare( strict_types=1 );
+
+namespace Tasker\Infrastructure\Projection\Task;
+
+use MongoDB\Database;
+use Tasker\Infrastructure\Projection\Table;
+use Tasker\Model\Task\Domain\TaskId;
+
+/**
+ * Class TaskFinder
+ * @package Tasker\Infrastructure\Projection\Task
+ */
+class TaskFinder
+{
+	/**
+	 * @var Database
+	 */
+	private $mongoConnection;
+
+	/**
+	 * TaskFinder constructor.
+	 * @param Database $mongoConnection
+	 */
+	public function __construct(Database $mongoConnection)
+	{
+		$this->mongoConnection = $mongoConnection;
+	}
+
+	/**
+	 * @param TaskId $id
+	 * @return array|null
+	 */
+	public function findById(TaskId $id)
+	{
+		$collection = $this->mongoConnection->selectCollection(Table::READ_MONGO_TASKS);
+		$filters = ['task_id' => $id->toString()];
+		$options = [
+			'typeMap' => [
+				'root' => 'array',
+				'document' => 'array',
+			]
+		];
+
+		$aggregate = [
+			['$match' => $filters],
+			['$limit' => 1],
+			[
+				'$lookup' => [
+					'from' => Table::MONGO_USERS_DISPLAY_NAMES,
+					'localField' => 'creator_id',
+					'foreignField' => 'user_id',
+					'as' => 'creatorArray'
+				],
+			],
+			[
+				'$project' => [
+					'_id' => 0,
+					'task_id' => 1,
+					'title' => 1,
+					'creator' => [
+						'id' => [
+							'$arrayElemAt' => ['$creatorArray.user_id', 0]
+						],
+						'name' => [
+							'$arrayElemAt' => ['$creatorArray.display_name', 0]
+						]
+					]
+				]
+			]
+		];
+
+		$tasks = $collection->aggregate($aggregate, $options)->toArray();
+		return $tasks ? $tasks[0] : null;
+	}
+}

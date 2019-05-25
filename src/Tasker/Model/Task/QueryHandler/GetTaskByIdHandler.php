103d376ac54bb798049ed519ab2e307412ee44be
Karol <dabrowski.karad@gmail.com>
2019-05-25 20:05:51 +0200

    Create handler for GetTaskById query

---
+++ src/Tasker/Model/Task/QueryHandler/GetTaskByIdHandler.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace Tasker\Model\Task\QueryHandler;
+
+use React\Promise\Deferred;
+use Tasker\Infrastructure\Projection\Task\TaskFinder;
+use Tasker\Model\Task\Query\GetTaskById;
+
+/**
+ * Class GetTaskByIdHandler
+ * @package Tasker\Model\Task\QueryHandler
+ */
+class GetTaskByIdHandler
+{
+	/**
+	 * @var TaskFinder
+	 */
+	private $taskFinder;
+
+	/**
+	 * GetTaskByIdHandler constructor.
+	 * @param TaskFinder $taskFinder
+	 */
+	public function __construct(TaskFinder $taskFinder)
+	{
+		$this->taskFinder = $taskFinder;
+	}
+
+	/**
+	 * @param GetTaskById $query
+	 * @param Deferred|null $deferred
+	 * @return array|null
+	 */
+	public function __invoke(GetTaskById $query, Deferred $deferred = null)
+	{
+		$response = $this->taskFinder->findById($query->taskId());
+
+		if (null === $deferred) {
+			return $response;
+		}
+
+		$deferred->resolve($response);
+	}
+}

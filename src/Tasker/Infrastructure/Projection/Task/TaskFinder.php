5a3f28bc8d49e6319a353787ae75e949626b7c10
Karol <dabrowski.karad@gmail.com>
2019-06-25 22:07:29 +0200

    Remove unnecessary functions from finder

--- src/Tasker/Infrastructure/Projection/Task/TaskFinder.php
+++ src/Tasker/Infrastructure/Projection/Task/TaskFinder.php
@@ -30,59 +30,18 @@ class TaskFinder
 
 	/**
 	 * @param TaskId $id
-	 * @return array|null
 	 */
 	public function findById(TaskId $id)
 	{
-		$collection = $this->mongoConnection->selectCollection(Table::READ_MONGO_TASKS);
-		$filters = ['task_id' => $id->toString()];
-
-		$aggregate = [
-			['$match' => $filters],
-			['$limit' => 1],
-			[
-				'$lookup' => [
-					'from' => Table::MONGO_USERS_DISPLAY_NAMES,
-					'localField' => 'creator_id',
-					'foreignField' => 'user_id',
-					'as' => 'creatorArray'
-				],
-			],
-			[
-				'$project' => [
-					'_id' => 0,
-					'task_id' => 1,
-					'title' => 1,
-					'creator' => [
-						'id' => [
-							'$arrayElemAt' => ['$creatorArray.user_id', 0]
-						],
-						'name' => [
-							'$arrayElemAt' => ['$creatorArray.display_name', 0]
-						]
-					]
-				]
-			]
-		];
-
-		$tasks = $collection->aggregate($aggregate, $this->getOptions())->toArray();
-		return $tasks ? $tasks[0] : null;
+		//@TODO Implement
 	}
 
 	/**
 	 * @param UserId $id
-	 * @return array|null
 	 */
 	public function findUserTasks(UserId $id)
 	{
-		$collection = $this->mongoConnection->selectCollection(Table::READ_MONGO_USER_TASKS);
-
-		$filter = [
-			'user_id' => $id->toString()
-		];
-
-		$user = $collection->findOne($filter, $this->getOptions());
-		return $user ? $user['tasks'] : null;
+		//@TODO Implement
 	}
 
 	/**

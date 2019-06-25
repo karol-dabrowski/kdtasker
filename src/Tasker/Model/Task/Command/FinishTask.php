f627152939d128b560ddb304c6ca8194e0781e93
Karol <dabrowski.karad@gmail.com>
2019-06-25 14:21:43 +0200

    Create FinishTask command

---
+++ src/Tasker/Model/Task/Command/FinishTask.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace Tasker\Model\Task\Command;
+
+use Prooph\Common\Messaging\Command;
+use Prooph\Common\Messaging\PayloadConstructable;
+use Prooph\Common\Messaging\PayloadTrait;
+use Tasker\Model\Task\Domain\TaskId;
+use Assert\Assertion;
+
+/**
+ * Class FinishTask
+ * @package Tasker\Model\Task\Command
+ */
+class FinishTask extends Command implements PayloadConstructable
+{
+	use PayloadTrait;
+
+	/**
+	 * @return TaskId
+	 */
+	public function taskId(): TaskId
+	{
+		return TaskId::fromString($this->payload['task_id']);
+	}
+
+	/**
+	 * @param array $payload
+	 */
+	protected function setPayload(array $payload): void
+	{
+		Assertion::keyExists($payload, 'task_id', 'task_id|is_required');
+		Assertion::uuid($payload['task_id'], 'task_id|must_be_correct_uuid');
+
+		$this->payload = $payload;
+	}
+}

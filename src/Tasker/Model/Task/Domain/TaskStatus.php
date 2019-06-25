48637fb057e51cbcbac9e40ce15d997d69a5b54c
Karol <dabrowski.karad@gmail.com>
2019-06-25 17:05:19 +0200

    Add TaskStatus class

---
+++ src/Tasker/Model/Task/Domain/TaskStatus.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Tasker\Model\Task\Domain;
+
+/**
+ * Class TaskStatus
+ * @package Tasker\Model\Task\Domain
+ */
+final class TaskStatus
+{
+	public const OPEN = 1;
+	public const COMPLETED = 2;
+	public const REMOVED = 3;
+}

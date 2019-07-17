09d4782b01b882161666f9ad0ea136f201dc3531
Karol <dabrowski.karad@gmail.com>
2019-07-17 15:44:10 +0200

    Fix task unit test and add new one for deadline without time

--- tests/unit/Task/TaskTest.php
+++ tests/unit/Task/TaskTest.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Task;
 
+use Faker\Factory;
+use Faker\Generator;
 use Tasker\Model\Task\Domain\Task;
+use Tasker\Model\Task\Domain\TaskDeadline;
 use Tasker\Model\Task\Domain\TaskId;
 use Tasker\Model\Task\Event\TaskCreated;
 use Tasker\Model\User\Domain\UserId;
@@ -18,6 +21,11 @@ class TaskTest extends \Codeception\Test\Unit
      * @var \App\Tests\UnitTester
      */
     protected $tester;
+
+	/**
+	 * @var Generator
+	 */
+	private $faker;
 
 	/**
 	 * @var TaskId
@@ -40,24 +48,37 @@ class TaskTest extends \Codeception\Test\Unit
 	private $assigneeId;
 
 	/**
+	 * @var TaskDeadline
+	 */
+	private $deadlineWithoutTime;
+
+	/**
+	 * @var TaskDeadline
+	 */
+	private $deadlineWithTime;
+
+	/**
 	 * @throws \Exception
 	 */
 	protected function _before()
 	{
+		$this->faker = Factory::create();
 		$this->taskId = TaskId::generate();
 		$this->taskTitle = "Test title";
 		$this->creatorId = UserId::generate();
 		$this->assigneeId = UserId::generate();
-
+		$this->deadlineWithoutTime = TaskDeadline::fromString($this->faker->date(), null);
+		$this->deadlineWithTime = TaskDeadline::fromString($this->faker->date(), $this->faker->time("H:i"));
 	}
 
-	public function testTaskCanBeCreated()
+	public function testTaskWithoutTimeCanBeCreated()
     {
-		$task = Task::create($this->taskId, $this->taskTitle, $this->creatorId, $this->assigneeId);
+		$task = Task::create($this->taskId, $this->taskTitle, $this->creatorId, $this->assigneeId, $this->deadlineWithoutTime);
 	    $this->assertSame($this->taskId, $task->taskId());
 	    $this->assertSame($this->taskTitle, $task->title());
 	    $this->assertSame($this->creatorId, $task->creatorId());
 	    $this->assertSame($this->assigneeId, $task->assigneeId());
+	    $this->assertSame($this->deadlineWithoutTime, $task->deadline());
 	    $this->assertInstanceOf(Task::class, $task);
 
 		$events = $this->tester->popRecordedEvents($task);
@@ -69,5 +90,28 @@ class TaskTest extends \Codeception\Test\Unit
 	    $this->assertSame($this->taskTitle, $event->title());
 		$this->assertSame($this->creatorId, $event->creatorId());
 		$this->assertSame($this->assigneeId, $event->assigneeId());
+		$this->assertSame($this->deadlineWithoutTime, $event->deadline());
     }
+
+	public function testTaskWithTimeCanBeCreated()
+	{
+		$task = Task::create($this->taskId, $this->taskTitle, $this->creatorId, $this->assigneeId, $this->deadlineWithTime);
+		$this->assertSame($this->taskId, $task->taskId());
+		$this->assertSame($this->taskTitle, $task->title());
+		$this->assertSame($this->creatorId, $task->creatorId());
+		$this->assertSame($this->assigneeId, $task->assigneeId());
+		$this->assertSame($this->deadlineWithTime, $task->deadline());
+		$this->assertInstanceOf(Task::class, $task);
+
+		$events = $this->tester->popRecordedEvents($task);
+		$this->assertCount(1, $events);
+
+		$event = $events[0];
+		$this->assertInstanceOf(TaskCreated::class, $event);
+		$this->assertSame($this->taskId, $event->taskId());
+		$this->assertSame($this->taskTitle, $event->title());
+		$this->assertSame($this->creatorId, $event->creatorId());
+		$this->assertSame($this->assigneeId, $event->assigneeId());
+		$this->assertSame($this->deadlineWithTime, $event->deadline());
+	}
 }
